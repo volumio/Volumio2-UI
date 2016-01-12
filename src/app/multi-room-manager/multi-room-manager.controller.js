195612290bb8c3a59efa2363cd26e465e6985ee6
Tommaso Rossi (kurtommy) <kurtommy@gmail.com>
2016-01-12 22:24:21 +0100

    Moved all multi room function from controller to service. Added volume control for group and child devices. To test when the back end will be updated

--- src/app/multi-room-manager/multi-room-manager.controller.js
+++ src/app/multi-room-manager/multi-room-manager.controller.js
@@ -1,55 +1,17 @@
 class MultiRoomManagerController {
-  constructor(socketService, multiRoomService) {
+  constructor(socketService, multiRoomService, $timeout) {
     'ngInject';
     this.socketService = socketService;
     this.multiRoomService = multiRoomService;
+    this.$timeout = $timeout;
   }
 
   // onDragComplete(data,event){
   // }
 
-  onDropComplete(from, to, event){
+  onDropComplete(from, to, event) {
     console.log(from, to);
-    // let fromIndex = this.multiRoomService.devices.list.indexOf(from),
-    //     toIndex = this.multiRoomService.devices.list.indexOf(to);
-    //
-    // if (this.multiRoomService.devices.list[toIndex].child) {
-    //   this.multiRoomService.devices.list[toIndex].child.push(from);
-    // } else {
-    //   this.multiRoomService.devices.list[toIndex].child = [from];
-    // }
-    // this.multiRoomService.devices.list.splice(fromIndex, 1);
-
-    //this.socketService.emit('setMultiRoomDevices', this.multiRoomService.devices.list);
-
-    // FIXME move into service
-    let obj = {
-      ip: from.ip,
-      set: 'client'
-    };
-    console.log('setClient', obj);
-    this.socketService.emit('setMultiroom', obj);
-    obj = {
-      ip: to.ip,
-      set: 'server'
-    };
-    console.log('setServer', obj);
-    this.socketService.emit('setMultiroom', obj);
-  }
-
-  removeChildDevice(device, childDevice){
-    // let deviceIndex = this.multiRoomService.devices.list.indexOf(device),
-    //     childDeviceIndex = this.multiRoomService.devices.list[deviceIndex].child.indexOf(childDevice);
-    // this.multiRoomService.devices.list[deviceIndex].child.splice(childDeviceIndex, 1);
-    // this.multiRoomService.devices.list.push(childDevice);
-
-    // FIXME move into service
-    let obj = {
-      ip: childDevice.ip,
-      set: 'single'
-    };
-    console.log('removeChildDevice', obj);
-    this.socketService.emit('setMultiroom', obj);
+    this.multiRoomService.addChild(from, to);
   }
 
   toggleClientsView(device) {
@@ -59,6 +21,26 @@ class MultiRoomManagerController {
       device.clientVisible = true;
     }
   }
+
+  changeGroupVolume(ip, volume) {
+    if (this.timeoutHandler) {
+      this.$timeout.cancel(this.timeoutHandler);
+    }
+    this.timeoutHandler = this.$timeout(() => {
+      this.multiRoomService.changeGroupVolume(ip, volume);
+    }, 300, false);
+  }
+
+  changeChildVolume(ip, volume) {
+    if (this.timeoutHandler) {
+      this.$timeout.cancel(this.timeoutHandler);
+    }
+    this.timeoutHandler = this.$timeout(() => {
+      this.multiRoomService.changeChildVolume(ip, volume);
+    }, 300, false);
+  }
+
+
 }
 
 export default MultiRoomManagerController;
