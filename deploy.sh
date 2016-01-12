7c5747594cf5ea28fcaf84505ccb5c157e81861e
Volumio <info@volumio.org>
2016-01-13 00:08:27 +0100

    Two deployments: one for dist and one for demo on gh-pages

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,12 @@ git config user.email "info@volumio.org"
 
 # Deploy
 git add .
-git commit -m "Deploy to GitHub Pages"
+git commit -m "Deploy to Dist Branch"
 
+# Force push from the current repo's master branch to the dist branch for deployment
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:dist > /dev/null 2>&1
+
+##TODO edit default IP to point to DEMO SERVER and push demo version to gh-pages
+#git commit -m "Deploy DEMO Version"
 # Force push from the current repo's master branch to the remote gh-page repo
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+#git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
