31b98b71f1b4c765e7ce637c0bb5cd96b1de9f78
Tommaso Rossi <kurtommy@gmail.com>
2016-05-16 16:08:19 +0200

    - Set background in UiSettingsService - Table hover color changed

--- src/app/services/ui-settings.service.js
+++ src/app/services/ui-settings.service.js
@@ -1,8 +1,9 @@
 class UiSettingsService {
-  constructor($rootScope, socketService, mockService, $log, themeManager) {
+  constructor($rootScope, socketService, mockService, $log, themeManager, $document) {
     'ngInject';
     this.socketService = socketService;
     this.themeManager = themeManager;
+    this.$document = $document;
     this.$log = $log;
 
     this.currentTheme = themeManager.theme;
@@ -27,8 +28,14 @@ class UiSettingsService {
     this.$log.debug('UiSettingsService is listening');
     this.socketService.on('pushUiSettings', (data) => {
       this.$log.debug('pushUiSettings', data);
-      //TODO chose merge strategy or pull entire set from BE
       this.uiSettings = data;
+      if (data.background.title === 'Default') {
+        this.$document[0].body.style.background = `#333 url(${data.background.path}) repeat top left`;
+        this.$document[0].body.style.backgroundSize = 'auto';
+      } else {
+        this.$document[0].body.style.background = `#333 url(${data.background.path}) no-repeat center center`;
+        this.$document[0].body.style.backgroundSize = 'cover';
+      }
     });
   }
 
