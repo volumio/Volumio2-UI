4563baf218fec4ca24892b9449dad9dff151b847
Tommaso Rossi <kurtommy@gmail.com>
2017-03-19 22:33:06 +0000

    - Use app name instead of page title

--- gulp/build.js
+++ gulp/build.js
@@ -130,7 +130,7 @@ gulp.task('replace-page-title', ['html'], function () {
   var pageTitle = themeSettings.pageTitle || 'Audiophile music player';
   var index = fs.readFileSync(`dist/index.html`, 'utf8');
   index = index.replace('<title></title>', `<title>${pageTitle}</title>`);
-  index = index.replace(/@APP_NAME/g, pageTitle);
+  index = index.replace(/@APP_NAME/g, themeSettings.app || '');
   fs.writeFileSync('dist/index.html', index);
 });
 
