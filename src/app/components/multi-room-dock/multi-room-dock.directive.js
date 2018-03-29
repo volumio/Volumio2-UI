b30f76f2219dd49427f20e0dd61091d20368a0f1
volumio <info@volumio.org>
2018-03-29 15:02:53 +0200

    Reload uisettings after multiroom change

--- src/app/components/multi-room-dock/multi-room-dock.directive.js
+++ src/app/components/multi-room-dock/multi-room-dock.directive.js
@@ -23,6 +23,7 @@ class MultiRoomDockController {
   changeDevice(device) {
     if (!device.isChild && !device.isSelf) {
       this.socketService.host = device.host;
+      this.socketService.emit('getUiSettings');
     }
   }
 }
