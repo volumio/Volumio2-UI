75840804105922f07c7345b1bb811f8abefe0996
volumio <info@volumio.org>
2018-05-17 01:23:42 +0200

    Usability tweaks cd

--- src/app/components/modals/modal-ripper.controller.js
+++ src/app/components/modals/modal-ripper.controller.js
@@ -1,14 +1,16 @@
 class ModalRipperController {
-  constructor($uibModalInstance, dataObj, ripperService) {
+  constructor($uibModalInstance, dataObj, ripperService, browseService) {
     'ngInject';
     this.$uibModalInstance = $uibModalInstance;
     this.ripperService = ripperService;
     this.dataObj = dataObj;
+    this.browseService = browseService;
   }
 
   startToRipCd() {
     this.ripperService.startToRipCd();
     this.$uibModalInstance.close();
+    this.browseService.backHome();
   }
 
   cancel() {
