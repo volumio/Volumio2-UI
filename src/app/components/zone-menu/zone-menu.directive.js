97b1a2a9790aae07d296afc2e348e65fa011cb6d
Abrar Mahmood <abrarmahmood@me.com>
2019-01-13 18:12:46 +0000

    Created zone menu with mock JSON data

--- src/app/components/zone-menu/zone-menu.directive.js
+++ src/app/components/zone-menu/zone-menu.directive.js
@@ -13,23 +13,49 @@ class ZoneMenuDirective {
   }
 }
 
+const mockData = {
+  "availableOutputs": [
+    {
+      "name": "Browser",
+      "type": "browser",
+      "icon": "fa fa-volume-up",
+      "available": true,
+      "enabled": "true",
+      "volumeAvailable": "true",
+      "volume": 50,
+      "mute": false
+    },
+    {
+      "name": "Volumio Kitchen",
+      "type": "device",
+      "icon": "fa fa-volume-up",
+      "available": true,
+      "enabled": "true",
+      "volumeAvailable": "true",
+      "volume": 30,
+      "mute": false
+    },
+    {
+      "name": "Volumio Studio",
+      "type": "device",
+      "icon": "fa fa-volume-up",
+      "available": true,
+      "enabled": "true",
+      "volumeAvailable": "true",
+      "volume": 92,
+      "mute": false
+    }
+  ]
+}
+
 class ZoneMenuController {
-  constructor($scope, $rootScope, socketService, mockService, $state, modalService, playerService, themeManager, $log,
-      $http, $window, uiSettingsService, authService) {
+  constructor($scope, $rootScope, socketService, $state, $log) {
     'ngInject';
     this.$state = $state;
-    this.$window = $window;
     this.socketService = socketService;
-    this.modalService = modalService;
-    this.playerService = playerService;
     this.visible = false;
-    this.themeManager = themeManager;
     this.$log = $log;
     this.$scope = $scope;
-    this.uiSettingsService = uiSettingsService;
-    // this.menuItems = mockService.get('getMenuItems');
-    this.authService = authService;
-    this.MYVOLUMIO_KEY = 'my-volumio';
 
     this.init();
     $rootScope.$on('socket:init', () => {
@@ -40,111 +66,35 @@ class ZoneMenuController {
     });
   }
 
-  toggleAnalogInput() {
-    this.socketService.emit('callMethod', {
-      endpoint: 'system_controller/gpios',
-      method: 'DASwitchPress'
-    });
-  }
-
-  toggleBluetooth() {
-    this.socketService.emit('callMethod', {
-      endpoint: 'audio_interface/bluetooth',
-      method: 'BTpress'
-    });
-  }
-
   toggleMenu() {
     this.visible = !this.visible;
   }
 
   itemClick(item) {
-    this.toggleMenu();
-    this.$log.debug(item);
-    if (item.id === 'modal') {
-      let
-        controllerName = item.params.modalName.split('-').map(
-          (item) => {
-            return item[0].toUpperCase() + item.slice(1, item.length);
-          }).join(''),
-        templateUrl = 'app/components/side-menu/elements/' +
-            item.params.modalName + '.html'  ;
-      this.$log.debug(controllerName);
-      this.modalService.openModal(
-        controllerName + 'Controller',
-        templateUrl,
-        item,
-        item.params.modalSize || 'lg');
-    } else if (item.id === 'link') {
-      this.$window.open(item.params.url);
-    } else if (item.id === 'static-page') {
-      this.$state.go('volumio.static-page', {pageName: item.pageName});
-    } else if (item.state) {
-      this.$log.debug(item.state, item.params);
-      if (item.params) {
-        for (let param in item.params) {
-          item.params[param] = String(item.params[param]).replace('/', '-');
-        }
-        this.$state.go(item.state, item.params);
-      } else {
-        this.$state.go(item.state);
-      }
-    } else {
-      this.$state.go(item.state);
-    }
+    this.$log.debug('Clicked on', item);
+    console.log('Clicked on', item);
   }
 
   init() {
     this.registerListner();
     this.initService();
-    this.watcherHandler = this.$scope.$watch(
-      () =>
-        this.playerService &&
-        this.playerService.state &&
-        this.playerService.state.service, (val) => {
-      if (val) {
-        this.analogIn = this.playerService.state.service === 'analogin';
-        this.bluetooth = this.playerService.state.service === 'bluetoth';
-      }
-    });
   }
 
   registerListner() {
-    this.socketService.on('pushMenuItems', (data) => {
-      this.$log.debug('pushMenuItems', data);
+    this.menuItems = mockData.availableOutputs;
+    this.socketService.on('pushZoneItems', (data) => {
+      this.$log.debug('pushZoneItems', data);
       this.menuItems = data;
-      this.checkEnableMyVolumio();
     });
 
     this.$scope.$on('$destroy', () => {
-      this.socketService.off('pushMenuItems');
+      this.socketService.off('pushZoneItems');
     });
   }
 
   initService() {
-    this.socketService.emit('getMenuItems');
+    this.socketService.emit('pushZoneItems');
   }
-
-  checkEnableMyVolumio(){
-    if(this.isAuthActive()){
-      this.authService.enableAuth();
-    }
-  }
-
-  isAuthActive(){
-    return this.isPluginActiveById('my-volumio');
-  }
-
-  isPluginActiveById(pluginId){
-    for(var i in this.menuItems){
-      var plugin = this.menuItems[i];
-      if(plugin.hasOwnProperty('id') && plugin.id === pluginId){
-        return true;
-      }
-    }
-    return false;
-  }
-  
 }
 
 export default ZoneMenuDirective;
