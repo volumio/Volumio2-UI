198c823bd6274c8aee2c4bf0fd5fe443b7862179
Abrar Mahmood <abrarmahmood@me.com>
2019-01-26 15:02:24 +0000

    Removed hard coded audio output list Browser playback service now fetches audio outputs

--- src/app/components/zone-menu/zone-menu.controller.js
+++ src/app/components/zone-menu/zone-menu.controller.js
@@ -1,79 +1,19 @@
 import { groupBy } from 'lodash';
 
-const MOCK_DATA = {
-  "availableOutputs": [
-    {
-      "name": "Browser",
-      "type": "browser",
-      "icon": "fa fa-volume-up",
-      "available": true,
-      "enabled": true,
-      "volumeAvailable": true,
-      "volume": 50,
-      "mute": false
-    },
-    {
-      "name": "Volumio Kitchen",
-      "type": "device",
-      "icon": "fa fa-volume-up",
-      "available": true,
-      "enabled": false,
-      "volumeAvailable": true,
-      "volume": 30,
-      "mute": true
-    },
-    {
-      "name": "Volumio Studio",
-      "type": "device",
-      "icon": "fa fa-volume-up",
-      "available": true,
-      "enabled": true,
-      "volumeAvailable": true,
-      "volume": 92,
-      "mute": false
-    },
-    {
-      "name": "Volumio Bedroom",
-      "type": "device",
-      "icon": "fa fa-volume-up",
-      "available": true,
-      "enabled": false,
-      "volumeAvailable": true,
-      "volume": 92,
-      "mute": false
-    },
-  ]
-};
 
 class ZoneMenuController {
-  constructor($scope, $rootScope, socketService, $state, $log, browserPlaybackService) {
+  constructor($scope, socketService, $state, $log, browserPlaybackService) {
     'ngInject';
     this.$state = $state;
     this.socketService = socketService;
     this.browserPlayerService = browserPlaybackService;
     this.$log = $log;
     this.$scope = $scope;
-    
-    this.init();
-    // $rootScope.$on('socket:init', () => this.init());
-    // $rootScope.$on('socket:reconnect', () => this.initService());
-    
+
+    this.registerServiceObserver();
+
     // UI state, updated by the service
     this.uiState = this.mapUIState(this.browserPlayerService.state);
-  }
-
-  mapUIState(serviceState) {
-    // Local component state
-    const defaultLocalState = {
-      menuVisible: false,
-    };
-
-    const mappedState = {
-      mute: serviceState.mute,
-      volume: serviceState.volume.toString(),
-    };
-
-    return Object.assign({}, defaultLocalState, this.uiState, mappedState);
   }
 
   registerServiceObserver() {
@@ -86,40 +26,31 @@ class ZoneMenuController {
     });
   }
 
-  init() {
-    this.registerListner();
-    this.initService();
-    this.registerServiceObserver();
+  mapUIState(serviceState) {
+    // Local component state
+    const defaultLocalState = {
+      menuVisible: false,
+    };
+
+    const mappedState = {
+      mute: serviceState.mute,
+      volume: serviceState.volume.toString(),
+      availableOutputs: groupBy(serviceState.availableOutputs, 'type'),
+    };
+
+    return Object.assign({}, defaultLocalState, this.uiState, mappedState);
   }
 
-  registerListner() {
-    this.menuItems = groupBy(MOCK_DATA.availableOutputs, 'type');
-    this.menuItemTypes = Object.keys(this.menuItems);
-
-    this.socketService.on('pushZoneItems', (data) => {
-      this.$log.debug('pushZoneItems', data);
-      this.menuItems = data;
-    });
-
-    this.$scope.$on('$destroy', () => {
-      this.socketService.off('pushZoneItems');
-    });
-  }
-
-  initService() {
-    this.socketService.emit('getZoneItems');
+  /* Manage the UI state here */
+  itemClick(item) {
+    this.$log.debug('Clicked on', item);
+    // Implement method here
   }
 
   toggleMenu() {
     this.uiState.menuVisible = !this.uiState.menuVisible;
   }
 
-  itemClick(item) {
-    this.$log.debug('Clicked on', item);
-    // Implement method here
-  }
-
-  /* Manage the UI state here */
   onVolumeChange() {
     const val = parseFloat(this.uiState.volume);
 
--- src/app/services/browser-playback.service.js
+++ src/app/services/browser-playback.service.js
@@ -4,10 +4,25 @@ import ObserverService from './utils/observer-service';
 
 const exampleTrack = 'http://techslides.com/demos/samples/sample.flac';
 
+
+/**
+ * https://github.com/goldfire/howler.js/issues/825#issuecomment-446322017
+ */
+Howl.prototype.changeSrc = function (newSrc) {
+  var self = this;
+  self.unload(true);
+  self._src = newSrc;
+  self.load();
+};
+
 class BrowserPlaybackService extends ObserverService {
-  constructor() {
+  constructor(socketService, $log) {
     'ngInject';
     super();
+    this.socketService = socketService;
+    this.$log = $log;
+    this.howlerSoundID = undefined;
+    this.prevServerState = {};
     this.sound = new Howl({
       src: [
         exampleTrack
@@ -19,25 +34,92 @@ class BrowserPlaybackService extends ObserverService {
     this.state = {
       mute: false,
       volume: 1.0,
+      availableOutputs: [],
     };
+
+    this.registerListener();
+    this.initService();
+  }
+
+  registerListener() {
+    this.socketService.on('pushAudioOutputs', (data) => {
+      this.$log.debug('pushAudioOutputs', data);
+      this.setAvailableOutputs(data.availableOutputs);
+    });
+  }
+
+  initService() {
+    this.socketService.emit('getAudioOutputs');
   }
 
   toggleMute() {
-    this.sound.mute(!this.state.mute);
-    this.updateState({ mute: !this.state.mute });
+    console.log('hit toggle mute');
+    // this.sound.mute(!this.state.mute);
+    // this.updateState({ mute: !this.state.mute });
   }
 
   setVolume(vol) {
-    this.sound.volume(vol);
-    this.updateState({ volume: vol });
+    console.log('hit set volume');
+    // this.sound.volume(vol);
+    // this.updateState({ volume: vol });
   }
 
-  syncServerState(serverState) {
-    if (serverState.status === 'play') {
-      this.sound.play();
-    } else if (serverState.status === 'pause') {
-      this.sound.pause();
-    }
+  setAvailableOutputs(data) {
+    this.updateState({ availableOutputs: data });
+  }
+
+  syncServerState(obj) {
+    console.log('hit sync state');
+    // const serverState = {
+    //   uri: obj.uri,
+    //   status: obj.status,
+    //   seek: obj.seek,
+    // };
+
+    // if (this.prevServerState.uri === serverState.uri &&
+    //   this.prevServerState.status === serverState.status &&
+    //   this.prevServerState.seek === serverState.seek) {
+    //   return;
+    // } else {
+    //   this.prevServerState = serverState;
+    // }
+
+    // const seekTime = parseInt((serverState.seek / 1000));
+
+
+    // if (serverState.status === 'play') {
+    //   if (this.sound._src !== serverState.uri) {
+    //     this.sound.changeSrc(serverState.uri);
+    //     console.log('change src');
+    //   }
+
+    //   if (this.sound.playing() === false) {
+    //     if (this.sound.state() !== 'loaded') {
+    //       const d1 = new Date();
+    //       let self = this; // boo
+    //       this.sound.once('load', function () {
+    //         const d2 = new Date();
+    //         const deltaSeconds = ((d2.getTime() - d1.getTime()) / 1000);
+
+    //         self.sound.seek(seekTime + deltaSeconds, self.howlerSoundID);
+    //         self.howlerSoundID = self.sound.play(self.howlerSoundID);
+    //         console.log('loaded play event');
+    //       });
+    //     } else {
+    //       this.sound.seek(seekTime, this.howlerSoundID);
+    //       this.howlerSoundID = this.sound.play(this.howlerSoundID);
+    //       console.log('already loaded play event');
+    //     }
+    //   } else {
+    //     this.sound.seek(seekTime, this.howlerSoundID);
+    //     console.log('seek');
+    //   }
+    // } else if (serverState.status === 'pause') {
+    //   this.sound.pause(this.howlerSoundID);
+    //   console.log('pause');
+    // } else {
+    //   console.log('fell out');
+    // }
   }
 }
 
