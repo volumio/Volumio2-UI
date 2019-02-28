712ba902fecd83fb10e27be936dd117c765b7644
Luca Bartolini <bartoluca91@gmail.com>
2019-02-28 17:02:26 +0100

    WIP cdAlbumArt

--- src/app/components/modals/modal-ripper.controller.js
+++ src/app/components/modals/modal-ripper.controller.js
@@ -1,10 +1,23 @@
 class ModalRipperController {
-  constructor($uibModalInstance, dataObj, ripperService, browseService) {
-    'ngInject';
+  constructor(
+    $uibModalInstance,
+    dataObj,
+    ripperService,
+    browseService,
+    playerService,
+    Upload,
+    socketService,
+    $log
+  ) {
+    "ngInject";
     this.$uibModalInstance = $uibModalInstance;
     this.ripperService = ripperService;
     this.dataObj = dataObj;
     this.browseService = browseService;
+    this.playerService = playerService;
+    this.socketService = socketService;
+    this.Upload = Upload;
+    this.$log = $log;
   }
 
   startToRipCd() {
@@ -14,7 +27,29 @@ class ModalRipperController {
   }
 
   cancel() {
-    this.$uibModalInstance.dismiss('cancel');
+    this.$uibModalInstance.dismiss("cancel");
+  }
+
+  uploadAlbumart() {
+    this.Upload.upload({
+      url: `${this.socketService.host}/backgrounds-upload`,
+      data: {filename: this.albumart}
+    }).then(
+      resp => {
+        this.uploadPercentage = false;
+      },
+      resp => {
+        this.uploadPercentage = false;
+        this.$log.debug("Error status: " + resp.status);
+      },
+      evt => {
+        this.uploadPercentage = parseInt((100.0 * evt.loaded) / evt.total);
+
+        if (this.uploadPercentage === 100) {
+          this.uploadPercentage = false;
+        }
+      }
+    );
   }
 }
 
