813ce7b769047ab53ede1b19acb04bc108514525
Giacomo Matthew Degl'Innocenti <info@giacomodeglinnocenti.com>
2019-04-15 18:02:48 +0200

    Added MyVolumio card in side bar

--- src/app/components/main-menu/main-menu.directive.js
+++ src/app/components/main-menu/main-menu.directive.js
@@ -16,9 +16,56 @@ class MainMenuDirective {
 }
 
 class MainMenuController {
-  constructor($state) {
+  constructor($state, $scope, $rootScope, socketService) {
     'ngInject';
     this.$state = $state;
+
+    //all this boiler-plate is WIP and should works on dedicated service (coupled from side-menu)
+    this.$scope = $scope;
+    this.$rootScope = $rootScope;
+    this.socketService = socketService;
+    this.menuItems = [];
+
+    this.init();
+    $rootScope.$on('socket:init', () => {
+      this.init();
+    });
+    $rootScope.$on('socket:reconnect', () => {
+      this.initService();
+    });
+  }
+
+  init() {
+    this.registerListner();
+    this.initService();
+  }
+
+  registerListner() {
+    this.socketService.on('pushMenuItems', (data) => {
+      this.menuItems = data;
+    });
+
+    this.$scope.$on('$destroy', () => {
+      this.socketService.off('pushMenuItems');
+    });
+  }
+
+  initService() {
+    this.socketService.emit('getMenuItems');
+  }
+
+  isAuthActive(){
+    return this.isPluginActiveById('my-volumio');
+  }
+
+  isPluginActiveById(pluginId){
+    for(var i in this.menuItems){
+      var plugin = this.menuItems[i];
+      if(plugin.hasOwnProperty('id') && plugin.id === pluginId){
+        return true;
+      }
+    }
+    return false;
   }
 }
 
