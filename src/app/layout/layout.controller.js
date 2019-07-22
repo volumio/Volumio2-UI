0c8d17d1dcb8871d230bb321e92689c1563fe742
Giacomo Matthew Degl'Innocenti <info@giacomodeglinnocenti.com>
2019-07-22 10:12:57 +0200

    Modified layout and footer UIs, added playback tabbar UI

--- src/app/layout/layout.controller.js
+++ src/app/layout/layout.controller.js
@@ -1,13 +1,24 @@
 class LayoutController {
-  constructor($state, $log, matchmediaService) {
+  constructor($state, $scope, themeManager, $log, matchmediaService) {
     'ngInject';
     this.$state = $state;
+    this.$scope = $scope;
+    this.themeManager = themeManager;
     this.$log = $log;
     this.matchmediaService = matchmediaService;
 
     this.wrapperColumnClass = '';
     
     this.initMainMenu();
+
+    this.updateTabbar();
+    $scope.$on('$locationChangeStart', (event, next, current) => {
+      this.updateTabbar();
+    });
+  }
+
+  updateTabbar(){
+    this.isWithPlaybackBar = this.$state.$current.name === 'volumio.playback' && this.themeManager.theme === 'volumio3';
   }
 
   initMainMenu(){
