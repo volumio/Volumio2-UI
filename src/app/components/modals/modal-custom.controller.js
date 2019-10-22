4e5a4bc67297fc116fcfa7ffe03c0cf92da6398d
volumio <info@volumio.org>
2019-10-23 00:41:31 +0200

    Add url opening on modals

--- src/app/components/modals/modal-custom.controller.js
+++ src/app/components/modals/modal-custom.controller.js
@@ -1,10 +1,11 @@
 class ModalCustomController {
-  constructor($uibModalInstance, dataObj, socketService, $translate) {
+  constructor($uibModalInstance, dataObj, socketService, $translate, $window) {
     'ngInject';
     this.$uibModalInstance = $uibModalInstance;
     this.dataObj = dataObj;
     this.socketService = socketService;
     this.$translate = $translate;
+    this.$window = $window;
   }
 
   ok() {
@@ -18,6 +19,8 @@ class ModalCustomController {
   btnClick(button) {
     if (button.emit) {
       this.socketService.emit(button.emit, button.payload);
+    } else if (button.url) {
+      this.$window.open(button.url, "_self");
     }
     this.$uibModalInstance.close();
   }
