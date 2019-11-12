050bb2bc2c427c04a79dee6bb26f1537c5dae7bf
volumio <info@volumio.org>
2019-11-12 23:04:51 +0100

    Add state parameter in modal

--- src/app/components/modals/modal-custom.controller.js
+++ src/app/components/modals/modal-custom.controller.js
@@ -1,11 +1,12 @@
 class ModalCustomController {
-  constructor($uibModalInstance, dataObj, socketService, $translate, $window) {
+  constructor($uibModalInstance, dataObj, socketService, $translate, $window, $state) {
     'ngInject';
     this.$uibModalInstance = $uibModalInstance;
     this.dataObj = dataObj;
     this.socketService = socketService;
     this.$translate = $translate;
     this.$window = $window;
+    this.$state = $state;
   }
 
   ok() {
@@ -21,6 +22,8 @@ class ModalCustomController {
       this.socketService.emit(button.emit, button.payload);
     } else if (button.url) {
       this.$window.open(button.url, "_self");
+    } else if (button.state) {
+      this.$state.go(button.state);
     }
     this.$uibModalInstance.close();
   }
