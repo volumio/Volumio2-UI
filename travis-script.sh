9547c642762de88a45d8c8131175dac4c0b215d5
Giacomo Triggiano <giacomotriggiano@gmail.com>
2020-03-19 11:29:40 +0100

    fix

--- travis-script.sh
+++ travis-script.sh
@@ -5,13 +5,9 @@ set -e
 
 # Build the classic UI and move it into "dist-volumio"
 npm run build:volumio && mv dist dist-volumio
-# Fallback to 127.0.0.1 (for Ui on non-networked systems)
-echo '{"localhost": "http://127.0.0.1:3000"}' > dist-volumio/app/local-config.json
 
 # Build the v3 UI and move it into "dist-volumio3"
 npm run build:volumio3 && mv dist dist-volumio3
-# Fallback to 127.0.0.1 (for Ui on non-networked systems)
-echo '{"localhost": "http://127.0.0.1:3000"}' > dist-volumio3/app/local-config.json
 
 # Copy the classic UI into the folder which will be deployed to Firebase
 cp -r dist-volumio firebase-hosting
@@ -25,11 +21,15 @@ git config --global user.email "info@volumio.org"
 
 # Push classic UI on 'dist' branch
 cd "${P_DIR}/dist-volumio"
+# Fallback to 127.0.0.1 (for Ui on non-networked systems)
+echo '{"localhost": "http://127.0.0.1:3000"}' > app/local-config.json
 git init && git add . && git commit -m "Deploy to dist branch"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:dist > /dev/null 2>&1
 
 # Push v3 UI on 'dist3' branch
 cd "${P_DIR}/dist-volumio3"
+# Fallback to 127.0.0.1 (for Ui on non-networked systems)
+echo '{"localhost": "http://127.0.0.1:3000"}' > app/local-config.json
 git init && git add . && git commit -m "Deploy to dist branch"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:dist3 > /dev/null 2>&1
 
