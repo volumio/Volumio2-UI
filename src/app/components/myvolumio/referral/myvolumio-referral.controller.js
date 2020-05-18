81a1829c98f33d516f357a645197c174d05f943e
Bence Husi <bence.husi@gmail.com>
2020-05-18 09:21:20 +0200

    Referral UI first steps

--- src/app/components/myvolumio/referral/myvolumio-referral.controller.js
+++ src/app/components/myvolumio/referral/myvolumio-referral.controller.js
@@ -1,14 +1,15 @@
 class MyVolumioReferralController {
-    constructor($scope, authService, user, $rootScope, growSurfService, $window, cloudService) {
+    constructor($scope, $state, authService, user, $rootScope, /* growSurfService, */ $window, cloudService, $translate) {
         'ngInject';
-
         this.$scope = $scope;
+        this.$state = $state;
         this.authService = authService;
         this.user = user;
         this.$rootScope = $rootScope;
-        this.growSurfService = growSurfService;
+        /* this.growSurfService = growSurfService; */
         this.$window = $window;
         this.cloudService = cloudService;
+        this.$translate = $translate;
 
         this.init();
     }
@@ -16,9 +17,23 @@ class MyVolumioReferralController {
     init() {
         console.log('Howdy...');
         console.log(this.growSurfService);
-        
+
         /* this.$window.addEventListener('grsfReady', () => {
         }); */
+    }
+
+    backToProfile() {
+        this.$state.go('myvolumio.profile');
+    }
+
+    copyLink() {
+        const linkField = document.getElementById('my-volumio-link-field');
+        try {
+            linkField.select();
+        } catch (err) {
+            console.log(err);
+            console.log('Couldnt select text');
+        }
     }
 }
 
