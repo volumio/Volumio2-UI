650ba5750518b2ba572c0943aae0c8371ca54a2b
Volumio <info@volumio.org>
2022-05-03 18:33:14 +0200

    working iframe

--- src/app/iframe-pages/iframe-page.controller.js
+++ src/app/iframe-pages/iframe-page.controller.js
@@ -1,9 +1,15 @@
 class IframePageController {
-  constructor($http, $log, $stateParams, themeManager, $scope, $sce) {
+  constructor($http, $log, $stateParams, themeManager, $scope, $sce, $state) {
     'ngInject';
 
     // We provide the url and whitelist it
     this.url = $sce.trustAsResourceUrl($stateParams.url);
+    this.$state = $state;
+
+  }
+
+  exitFromIframePage() {
+    this.$state.go('volumio.playback');
   }
 }
 
