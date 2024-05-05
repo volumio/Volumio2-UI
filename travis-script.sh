a43e03d6cd121dab374eb766805ade2076024d12
Volumio <info@volumio.org>
2024-05-05 10:47:12 +0200

    Tweak to travis script

--- travis-script.sh
+++ travis-script.sh
@@ -19,23 +19,31 @@ git config --global user.email "info@volumio.org"
 # Push classic UI on 'dist' branch
 echo "Preparing dist branch"
 cd "dist-volumio"
+
 # Fallback to 127.0.0.1 (for Ui on non-networked systems)
 echo "Writing local-config.json"
 echo '{"localhost": "http://127.0.0.1:3000"}' > app/local-config.json
+
 echo "Initializing Git Repo"
 git init && git add . && git commit -m "Deploy to dist branch"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:dist > /dev/null 2>&1
+
+echo "Pushing to dist branch"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:dist
 
 cd .. 
 
 # Push v3 UI on 'dist3' branch
 echo "Preparing dist3 branch"
 cd "dist-volumio3"
+
 # Fallback to 127.0.0.1 (for Ui on non-networked systems)
 echo "Writing local-config.json"
 echo '{"localhost": "http://127.0.0.1:3000"}' > app/local-config.json
+
 echo "Initializing Git Repo"
 git init && git add . && git commit -m "Deploy to dist3 branch"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:dist3 > /dev/null 2>&1
+
+echo "Pushing to dist3 branch"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:dist3
 
 cd ..
